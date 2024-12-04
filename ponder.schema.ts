f6afaed31d9c0e0830ec25716a2191aa00ce0bb1
maxwell-1999 <maxwell@buffer.finance>
2024-12-04 16:05:00 +0530

    WIP

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -2,12 +2,5 @@ import { index, onchainTable, primaryKey, relations } from "@ponder/core";
 
 export const account = onchainTable("account", (t) => ({
   address: t.hex().primaryKey(),
-  bfrAamount: t.bigint().notNull().default(0n),
-  esBFRAamount: t.bigint().notNull().default(0n),
-}));
-
-export const vesterList = onchainTable("vester", (t) => ({
-  address: t.hex().primaryKey(),
-  v1balance: t.bigint().notNull().default(0n),
-  v2balance: t.bigint().notNull().default(0n),
+  fsBLPBalance: t.bigint().notNull().default(0n),
 }));
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,18 @@
-import { vesterList } from "./../ponder.schema";
 import { ponder } from "@/generated";
 import { account } from "../ponder.schema";
 
 ponder.on("BFR:Transfer", async ({ event, context }) => {
   await context.db
     .insert(account)
-    .values({ address: event.args.from, bfrAamount: 0n, esBFRAamount: 0n })
+    .values({ address: event.args.from, fsBLPBalance: 0n })
     .onConflictDoUpdate((row) => ({
-      bfrAamount: row.bfrAamount - event.args.amount,
+      fsBLPBalance: row.fsBLPBalance - event.args.amount,
     }));
 
   await context.db
     .insert(account)
-    .values({ address: event.args.to, bfrAamount: 0n, esBFRAamount: 0n })
+    .values({ address: event.args.to, fsBLPBalance: 0n })
     .onConflictDoUpdate((row) => ({
-      bfrAamount: row.bfrAamount + event.args.amount,
-    }));
-});
-ponder.on("esBFR:Transfer", async ({ event, context }) => {
-  await context.db
-    .insert(account)
-    .values({ address: event.args.from, bfrAamount: 0n, esBFRAamount: 0n })
-    .onConflictDoUpdate((row) => ({
-      esBFRAamount: row.esBFRAamount - event.args.amount,
-    }));
-
-  await context.db
-    .insert(account)
-    .values({ address: event.args.to, bfrAamount: 0n, esBFRAamount: 0n })
-    .onConflictDoUpdate((row) => ({
-      esBFRAamount: row.esBFRAamount + event.args.amount,
-    }));
-});
-ponder.on("Vester1:Deposit", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.account, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v1balance: row.v1balance + event.args.amount,
-    }));
-});
-ponder.on("Vester1:Withdraw", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.account, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v1balance: row.v1balance - event.args.balance,
-    }));
-});
-ponder.on("Vester1:Claim", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.receiver, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v1balance: row.v1balance - event.args.amount,
-    }));
-});
-ponder.on("Vester2:Deposit", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.account, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v2balance: row.v2balance + event.args.amount,
-    }));
-});
-ponder.on("Vester2:Withdraw", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.account, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v2balance: row.v2balance - event.args.balance,
-    }));
-});
-ponder.on("Vester2:Claim", async ({ event, context }) => {
-  await context.db
-    .insert(vesterList)
-    .values({ address: event.args.receiver, v1balance: 0n, v2balance: 0n })
-    .onConflictDoUpdate((row) => ({
-      v2balance: row.v2balance - event.args.amount,
+      fsBLPBalance: row.fsBLPBalance + event.args.amount,
     }));
 });
